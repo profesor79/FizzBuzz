c51069a38e5135959b93934b2e30878d051fbc06
Greg <you@example.com>
2023-03-22 17:05:49 +0100

    17:05:49.20

--- null.cs
+++ null.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    internal class @null
+    public class _null
     {
+        private static string[] _dividers3 = new string[] { "f", string.Empty, string.Empty };
+        private static string[] _dividers5 = new string[] { "bu", string.Empty, string.Empty, string.Empty, string.Empty };
+        private static string[] _results = new string[] { "", "fizz", "buzz", "fizzbuzz" };
+
+        public string FizzBuzz(int i)
+        {
+            _results[0] = i.ToString();
+            var s = _dividers3[i % 3] + _dividers5[i % 5];
+            return _results[s.Length];
+        }
     }
 }
