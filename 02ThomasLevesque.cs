c5af509f18f47ea27fdc711263f93af36d5382c1
Greg <you@example.com>
2023-03-22 14:42:09 +0100

    14:42:09.25

--- 02ThomasLevesque.cs
+++ 02ThomasLevesque.cs
@@ -13,13 +13,13 @@ namespace FizzBuzz
             bool fizz = i % 3 == 0;
             bool buzz = i % 5 == 0;
             if (fizz && buzz)
-                return "FizzBuzz");
-                else if (fizz)
-                return "Fizz");
-                else if (buzz)
-                return "Buzz");
-                else
-                return i);
+                return "FizzBuzz";
+            else if (fizz)
+                return "Fizz";
+            else if (buzz)
+                return "Buzz";
+            else
+                return i.ToString();
         }
     }
 }
