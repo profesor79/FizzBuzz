2dc137874dc71e889e0a2aa84f23cc82791c953c
Greg <you@example.com>
2023-03-22 16:29:44 +0100

    16:29:44.16

--- Xela.cs
+++ Xela.cs
@@ -8,5 +8,9 @@ namespace FizzBuzz
 {
     internal class Xela
     {
-    }
+        public string FMB
+        {
+            Enumerable.Range(1, 100).private ToList().private ForEach(i => Console.WriteLine(i % 3 * i % 5 == 0 ? (i % 3 == 0 ? "Fizz" : "") + (i % 5 == 0 ? "Buzz" : "") : i.ToString()));
+        }
 }
+    }
