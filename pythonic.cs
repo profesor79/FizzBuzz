3099fc75e42dbcfcb4759a9d4cd8e2263a62f0d6
Greg <you@example.com>
2023-03-22 16:54:49 +0100

    16:54:49.15

--- pythonic.cs
+++ pythonic.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    public class pythonic
+    public class _04pythonic
     {
+        private string[] s = new string[6] { "Fizz", "Buzz", "", "", "", "" };
+
+        public string FizzBuzz(int i)
+        {
+            string output = s[(i % 3) * 2] + s[(i % 5) + 1];
+            return string.IsNullOrEmpty(output) ? "" + i : output;
+        }
     }
 }
